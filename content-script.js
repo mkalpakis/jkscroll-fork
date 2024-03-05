6d805d165471d31a41f576cf24851fd7198100d4
mkalpakis <48073036+mkalpakis@users.noreply.github.com>
2024-03-05 18:40:57 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -4,9 +4,9 @@ addEventListener('keydown', event => {
   switch(event.key){
     case 'j': scrollBy(0, 50); break;
     case 'k': scrollBy(0, -50); break;
-    /* case 'h': history.back(); break; // dont think I want these, so removed them
+    case 'h': history.back(); break;
     case 'l': history.forward(); break;
-    case 't': chrome.runtime.sendMessage('t'); break;
+    /* case 't': chrome.runtime.sendMessage('t'); break; // dont want these so commented it out
     case 'w': chrome.runtime.sendMessage('w'); break; */
   }
 })
