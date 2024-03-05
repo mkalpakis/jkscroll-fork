97c676b7fb790d5e4f4ca3be42c8990074ccb06d
mkalpakis <48073036+mkalpakis@users.noreply.github.com>
2024-03-05 18:40:28 -0500

    Update content-script.js
    removed all features except for scrolling

--- content-script.js
+++ content-script.js
@@ -4,9 +4,9 @@ addEventListener('keydown', event => {
   switch(event.key){
     case 'j': scrollBy(0, 50); break;
     case 'k': scrollBy(0, -50); break;
-    case 'h': history.back(); break;
+    /* case 'h': history.back(); break; // dont think I want these, so removed them
     case 'l': history.forward(); break;
     case 't': chrome.runtime.sendMessage('t'); break;
-    case 'w': chrome.runtime.sendMessage('w'); break;
+    case 'w': chrome.runtime.sendMessage('w'); break; */
   }
 })
